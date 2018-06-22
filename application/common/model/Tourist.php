db5700d62719b8e1fd501690a90e50d8fae834d9
Nomango <569629550@qq.com>
2018-06-23 02:59:47 +0800

    商城页后端实现 & 各种BUG修复

---
+++ application/common/model/Tourist.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nomango
+ * Date: 2018-6-23
+ * Time: 1:25
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class Tourist extends Model
+{
+
+}
--- application/index/controller/TouristController.php
+++ application/index/controller/TouristController.php
@@ -9,6 +9,7 @@
 namespace app\index\controller;
 
 
+use app\common\model\Tourist;
 use think\Controller;
 use think\Request;
 
@@ -24,11 +25,25 @@ class TouristController extends Controller
     }
 
     public function landing() {
+        $tourist1 = Tourist::where('category', '=', 1)->select();
+        $tourist2 = Tourist::where('category', '=', 2)->select();
+        $tourist3 = Tourist::where('category', '=', 3)->select();
+
+        $this->assign([
+            'tourist1' => $tourist1,
+            'tourist2' => $tourist2,
+            'tourist3' => $tourist3
+        ]);
         return $this->fetch('pc/touristMall');
     }
 
     //详细旅游
     public function index() {
+        $tourist = Tourist::paginate(9);
+        $this->assign([
+            'tourist' => $tourist,
+        ]);
+
         if (Request::instance()->isMobile()) {
             return $this->fetch('index/detailTouristMall');
         } else {
@@ -38,6 +53,12 @@ class TouristController extends Controller
 
     //每一个旅游
     public function detail(){
+        $id = Request::instance()->param('id/d');
+        $tourist = Tourist::get($id);
+        $this->assign([
+            'tourist' => $tourist,
+        ]);
+
         if (Request::instance()->isMobile()) {
             return $this->fetch('index/detailTourist');
         } else {
